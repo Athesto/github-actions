b015b59103cef92942a9f083abf020be8f35351b
Athesto <gamez.live@gmail.com>
2022-10-04 20:44:43 -0500

    CHROMEWEBDRIVER

--- 01-google.py
+++ 01-google.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from selenium import webdriver
-driver = webdriver.Chrome()
+import os
+
+driver = webdriver.Chrome(os.environ['CHROMEWEBDRIVER'])
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 driver.quit()
